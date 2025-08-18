ac532d0042c27a53524816e375730148245daee3
lucamaio <lucos.maio@gmail.com>
2025-08-18 12:40:39 +0200

    Nuovo stile homepage

--- obj/Debug/net9.0/BonusIdrici2.AssemblyInfo.cs
+++ obj/Debug/net9.0/BonusIdrici2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BonusIdrici2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b7f130a5068bb023251fe7f31b4dca7c5403c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87d2a9def27dc8f1251f7df7ae0908dce353bb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BonusIdrici2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BonusIdrici2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
