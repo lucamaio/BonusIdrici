6561b0b81c667e0f3360d22d801ef2d0ae147b37
lucamaio <lucos.maio@gmail.com>
2025-08-25 08:53:03 +0200

    modifiche varie anagrafe

--- obj/Debug/net9.0/BonusIdrici2.AssemblyInfo.cs
+++ obj/Debug/net9.0/BonusIdrici2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BonusIdrici2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+887de4233e407b63e22e183314c0aad897f14ccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5154612535bc8ac8a852bc20500a8366846d5dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BonusIdrici2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BonusIdrici2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
